31e9708110022874138f487d0b5b638d5128b8ce
AchroDev <ztblaine@protonmail.com>
2024-04-14 16:22:31 -0500

    Fixed the main fn being wrapped in the base code's fn and changed x to mutable so it can be reassigned

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
+// Initial code
 fn main() {
-    // Initial code
-    fn main() {
-        let x = 5;
-        println!("The value of x is: {x}");
-        x = 6;
-        println!("The value of x is: {x}");
-    }
+   let mut x = 5;
+   println!("The value of x is: {x}");
+   x = 6;
+   println!("The value of x is: {x}");
 }

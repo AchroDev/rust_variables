43c482f5b550ff0fba11d15b13e3e6891ae09c33
AchroDev <ztblaine@protonmail.com>
2024-04-17 22:20:03 -0500

    Added notes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 // Shadowing Example
 
 fn main() {
+      // Initial x value
       let x = 5;
   
+      // Modified x value (What the compiler will see first)
       let x = x + 1;
-  
+      
+      // Inner scope
       {
+         // Modified x value in the inner scope
           let x = x * 2;
           println!("The value of x in the inner scope is: {x}");
       }

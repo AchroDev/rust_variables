09ef2c91dfbc2663bacf65cd31dc55eaf90c53d0
AchroDev <ztblaine@protonmail.com>
2024-04-17 22:17:54 -0500

    Changed to the Shadowing example code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-// Initial code
-fn main() {
-   let mut x = 5;
-   
-   const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+// Shadowing Example
 
-   println!("The value of x is: {x}");
-   x = 6;
-   println!("The value of x is: {x}");
+fn main() {
+      let x = 5;
+  
+      let x = x + 1;
+  
+      {
+          let x = x * 2;
+          println!("The value of x in the inner scope is: {x}");
+      }
+  
+      println!("The value of x is: {x}");
 }

b20d007255f7c0f576ec06d2b72303eca196690a
AchroDev <ztblaine@protonmail.com>
2024-04-19 01:27:01 -0500

    Added operations function with example variables

--- src/main.rs
+++ src/main.rs
@@ -21,8 +21,29 @@ Byte (u8 only)	b'A'
 
 // Ignores unused warning
 #![allow(unused)]
+
+use std::collections::btree_set::Difference;
 fn main() {
     let x = 2.0; //f64
 
     let y: f32 = 3.0; //f32
+}
+
+// example function of mathematical operations in Rust
+fn operations() {
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let diffference = 95.5 -4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quiotent = 56.7 / 32.2;
+    let truncated = -5 / 3; // Results in -1
+
+    // remainder
+    let remainder = 43 % 5;
 }

7d2366ad91c8cc4eb502b21a2c8c6de1a121c740
AchroDev <ztblaine@protonmail.com>
2024-04-19 01:42:22 -0500

    Added comment for reminder about Unicode Scalar Value ranges

--- src/char.rs
+++ src/char.rs
@@ -4,4 +4,10 @@ fn main() {
     let z: char = 'ℤ'; // with explicit type annotation
     let heart_eyed_cat = '😻';
     let emptyspace = ' '; // example of empty-width space acceptable as char type
+
+    /*Accented letters; Chinese, Japanese, and Korean characters; emoji; and zero-width -
+    - spaces are all valid char values in Rust.
+    Unicode Scalar Values range from U+0000 to U+D7FF and U+E000 to U+10FFFF inclusive.
+    */ 
+
 }

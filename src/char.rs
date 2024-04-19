10d5fde44208033e535fb2363f670771f798fd9f
AchroDev <ztblaine@protonmail.com>
2024-04-19 01:39:20 -0500

    Added example of empty-width space char type to char.rs

--- src/char.rs
+++ src/char.rs
@@ -3,4 +3,5 @@ fn main() {
     let c = 'z';
     let z: char = 'ℤ'; // with explicit type annotation
     let heart_eyed_cat = '😻';
+    let emptyspace = ' '; // example of empty-width space acceptable as char type
 }

6a546aef4ffc0028fb13c54d9ede5fbd2618548d
AchroDev <ztblaine@protonmail.com>
2024-04-19 01:19:50 -0500

    Added Integer Types and Literals, Set flag to ingore the warning for unused code

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
-// Shadowing Example
+// Data Types
 
+/* Integer Types
+Length	Signed	Unsigned
+8-bit	i8	    u8
+16-bit	i16	    u16
+32-bit	i32	    u32
+64-bit	i64	    u64
+128-bit	i128	u128
+arch	isize	usize
+*/
+
+/* Integer Literals
+Number literals	Example
+Decimal	        98_222
+Hex	            0xff
+Octal	        0o77
+Binary	        0b1111_0000
+Byte (u8 only)	b'A'
+*/
+
+// Ignores unused warning
+#![allow(unused)]
 fn main() {
-      // Initial x value
-      let x = 5;
-  
-      // Modified x value (What the compiler will see first)
-      let x = x + 1;
-      
-      // Inner scope
-      {
-         // Modified x value in the inner scope
-          let x = x * 2;
-          println!("The value of x in the inner scope is: {x}");
-      }
-  
-      println!("The value of x is: {x}");
+    let x = 2.0; //f64
+
+    let y: f32 = 3.0; //f32
 }

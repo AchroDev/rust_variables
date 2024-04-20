83d69ab09766b6cbc3f75e13f6e98f71cfa0474a
AchroDev <ztblaine@protonmail.com>
2024-04-20 01:41:43 -0500

    Fixed typo in compute

--- src/overflow_example.rs
+++ src/overflow_example.rs
@@ -6,7 +6,7 @@
 fn main() {
     // defining the initial u8 variable x
     let x: u8 = 0;
-    // causing an overflow by trying to computer 0_u8 - 1_u8, which is out of bounds
+    // causing an overflow by trying to compute 0_u8 - 1_u8, which is out of bounds
     let test = x - 1;
     // print result to console
     print!("{test}");

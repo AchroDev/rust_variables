7d10fb28883b210d52df3d74972fb6e221ee3bc9
AchroDev <ztblaine@protonmail.com>
2024-04-25 22:56:46 -0500

    Added Tuple Type example code

--- src/compound_types/tuple.rs
+++ src/compound_types/tuple.rs
@@ -1,1 +1,11 @@
+// Example of the Tuple Type
 
+/*
+*   A tuple is a general way of grouping together a number of values with a variety
+*   of types into one compound type. Tuples have a fixed length: once declared,
+*   they cannot grow or shrink in size.
+*/
+
+fn main() {
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+}

8e4bb9d172efc7c689ffb7568f5a41860aca9a6a
AchroDev <ztblaine@protonmail.com>
2024-04-26 01:53:49 -0500

    Added invalid_array.rs with an example of trying to access outside the array length

---
+++ src/compound_types/invalid_array.rs
@@ -0,0 +1,28 @@
+/*
+*   This is an example of what happens when you try to access an element of
+*   an array that is past the end of the array. Which will result in a OOB error
+*   causing the program to panic.
+*/
+
+use std::io;
+
+fn main() {
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
+}

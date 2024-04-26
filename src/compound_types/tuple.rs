dc10a80249e2d3027d2ac7df1fd0d0b150afcc1a
AchroDev <ztblaine@protonmail.com>
2024-04-26 00:47:18 -0500

    Seperated empty tuple example from the direct access example fn

--- src/compound_types/tuple.rs
+++ src/compound_types/tuple.rs
@@ -42,10 +42,13 @@ fn tuple_directAccess() {
     let six_point_four = x.1;
 
     let one = x.2;
+}
 
-    /*  A tuple without any values is called a "unit" which is written as "()"
-     *   and returns an empty value or an empty return type.
-     */
+/*  A tuple without any values is called a "unit" which is written as "()"
+ *   and returns an empty value or an empty return type.
+*/
+
+fn tuple_empty() {
     let tup1: () = ();
 
     let tup2: (unit, unit, unit) = ();

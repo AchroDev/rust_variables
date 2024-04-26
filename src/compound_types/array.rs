bb25831fc61dee67ba4dad15c37f12e7766467c0
AchroDev <ztblaine@protonmail.com>
2024-04-26 01:20:00 -0500

    Added an example array including the months in a year

--- src/compound_types/array.rs
+++ src/compound_types/array.rs
@@ -7,3 +7,24 @@
 fn main() {
     let array = [1, 2, 3, 4, 5];
 }
+
+// Arrays are useful when you want your data allocated on the stack rather
+// than the heap (This is discussed further in Chapter 4 - Ownership) or
+// when you want to ensure you always have a fixed number of elements.
+
+fn months() {
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+}
